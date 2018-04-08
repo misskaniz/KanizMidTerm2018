3aa590e9f49e3d71eb934c3c9f67650e130a08a6
Kaniz Ruma <kaniz.ruma2012@gmail.com>
2018-04-08 15:49:18 -0400

    Fibbonaci

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -1,11 +1,22 @@
 package math;
 
 public class Fibonacci {
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
          /*
           Write 40 Fibonacci numbers with java.
          */
 
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 1;
+        for (int i = 0; i < 40; i++) {
+            System.out.println(x);
+            x = x + y;
+            y = x - y;
+        }
 
     }
 }
+
+
+
